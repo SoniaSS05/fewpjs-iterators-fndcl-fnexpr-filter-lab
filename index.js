f14df940b9a26cd43823f097f29c3e4efe182330
Sonia <serrano_sonia@hotmail.com>
2021-04-06 17:42:58 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+const findMatching = (driv,lookF)=>{
+    const nuevo = driv.filter(elem => elem.toUpperCase() === lookF.toUpperCase());
+    return nuevo;
+}
+
+function fuzzyMatch(driv,lookF){
+   const nuevo = [];
+   let lookFlen = lookF.length;
+   for (const elem of driv){
+       let cad = elem.substr(0,lookFlen);
+       if (cad === lookF){
+            nuevo.push(elem);
+       }
+    }
+    return nuevo;
+}
+
+function matchName(driv, lookF){
+    const nuevo = [];
+    for (const element of driv) {
+        if (lookF === element.name) {
+           nuevo.push(element);
+        }
+    }
+    return nuevo;
+}
+
+
+
+
+
+ 
